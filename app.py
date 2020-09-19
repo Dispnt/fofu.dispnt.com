e157de93473acf2889ded3037cbf8dc5092b378d
dispnt <yuyuanjiajian@gmail.com>
2020-09-19 20:46:14 +0800

    app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+import requests
+
+steamKey = ""
+steamID = ""
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    steamProfile = requests.get(
+        'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' + steamKey + '&steamids=' + steamID).json()
+    steamNickname = steamProfile['response']['players'][0]['personaname']
+    if app.debug:
+        return render_template('fofu.html', debug=True, steamName=steamNickname)
+    else:
+        return render_template('fofu.html', debug=False, steamName=steamNickname)
+
+@app.route('/AR')
+def arroute():
+    return render_template('AR.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
